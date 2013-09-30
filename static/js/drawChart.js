6e1dbdf3cba396ea5138863c336a132f3e00ea13
vanhong <vanhongkao@Kaoteki-MacBook-Pro.local>
2013-09-30 22:54:58 +0800

    high charts 0930
    draw successful

---
+++ static/js/drawChart.js
@@ -0,0 +1,117 @@
+(function(Draw) {
+
+    function Tool() {
+
+        var doFilter = function() {
+            $.post('{% url "dofilter" %}' , 
+                function(data, status) { 
+                    alert("after load, get data=" + data + ", status=" + status); 
+                    $('#FilterResult').css('display','block');
+                }).fail(function(xhr){alert('error=' + xhr.statusText);});
+        }
+
+        var GetData = function(url) {
+            $.get(url, function(jData){
+                //alert('finish');
+                //$('#Result').html(jData);
+                $('#Result').highcharts({
+                    chart: {
+                        zoomType: 'xy'
+                    },
+                    title: jData.title,
+                    subtitle: jData.subtitle,
+                    xAxis: [{
+                            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], 
+                            max:23,
+                            labels: {
+                                step: 3
+                            }
+                    }],
+                    yAxis: [{ // Primary yAxis
+                        labels: {
+                            format: '{value}',
+                            style: {
+                                color: '#89A54E'
+                            }
+                        },
+                        title: {
+                            style: {
+                                color: '#89A54E'
+                            }
+                        }
+                    }, { // Secondary yAxis
+                        title: {
+                            style: {
+                                color: '#4572A7'
+                            }
+                        },
+                        labels: {
+                            format: '{value} 仟元',
+                            style: {
+                                color: '#4572A7'
+                            }
+                        },
+                        opposite: true
+                    }],
+                    tooltip: {
+                        shared: true
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'left',
+                        x: 120,
+                        verticalAlign: 'top',
+                        y: 100,
+                        floating: true,
+                        backgroundColor: '#FFFFFF'
+                    },
+                    series: [{
+                        name: '營收',
+                        color: '#4572A7',
+                        type: 'column',
+                        yAxis: 1,
+                        data: jData.revenue,
+                        tooltip: {
+                            valueSuffix: '(仟元)'
+                        }
+                    }, {
+                        name: '成長率',
+                        color: '#89A54E',
+                        type: 'spline',
+                        data: jData.growth_rate,
+                        tooltip: {
+                            valueSuffix: '%'
+                        }
+                    }],
+                    scrollbar: {
+                        enabled: true
+                    }
+                });    
+            });
+        }
+
+        this.Init = function() {
+            //alert("Init");
+            $(window).load(function() {
+                //Default figure
+                GetData('/getRevenueChart/');
+
+                $('#revenue').click(function(){
+                    GetData('url2');
+                    alert('revenue data');
+                });
+    			$('#Data2').click(function(){
+                    //FilterResult
+                    GetData('url2');
+                });
+
+                $('#Data3').click(function(){
+                    //FilterResult
+                    GetData('url3');
+                });
+            });
+        }
+    }
+    Draw.Tool = Tool;
+} (window.Draw = window.Draw || {}));
--- stock_analysis/views.py
+++ stock_analysis/views.py
@@ -117,117 +117,15 @@ def ajax_user_search(request):
 def index(request):
 	return HttpResponse('index')
 
-def test2(request):
-	return render_to_response('test4.html', context_instance = RequestContext(request))
-
-def getJSON2(request):
+def getRevenueChart(request):
 	symbol = '8114'
 	if StockId.objects.filter(symbol=symbol):
 		month_revenues = MonthRevenue.objects.filter(symbol=symbol).order_by('surrogate_key')
 		revenue_data = []
+		growth_rate_data = []
 		for revenue in month_revenues:
-			revenue_data.append(int(revenue.revenue))
-
-	data = {'revenue' : revenue_data}
-
-	json.dumps(data)
-
-
-	response_data = {'Tokyo' :  [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
-					'NewYork' : [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]}
+			if revenue.revenue is not None and revenue.year_growth_rate is not None:
+				revenue_data.append(int(revenue.revenue))
+				growth_rate_data.append(int(revenue.year_growth_rate))
+	data = {'revenue' : revenue_data, 'growth_rate' : growth_rate_data}
 	return HttpResponse(json.dumps(data), content_type="application/json")
-
-
-def getJSON(request):
-	print('in getJSON')
-	tokyo = ChartData('Tokyo', [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6])
-	newyork = ChartData('NewYork', [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5])
-	data = []
-	data.append(tokyo.__toDict__())
-	data.append(newyork.__toDict__())
-	'''resDataList = []
-	resData = ChartData('Tokyo', [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6])
-	resDataList.append(resData)
-	resData2 = ChartData('NewYork',[-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5])
-	resDataList.append(resData2)'''
-	response_data = {'Tokyo' :  [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
-					'NewYork' : [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]}
-	data2 = {'series' :[{ 'name' : 'Rainfall', 'color': '#4572A7','type': 'column','yAxis': 1,
-              'data': [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-              'tooltip' : { 'valueSuffix' : ' mm'} },
-			{ 'name' : 'Rainfall','color': '#89A54E', 'type': 'spline',
-              'data': [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
-              'tooltip': { 'valueSuffix': 'C' } }]}
-	data3 = {
-		'name' : 'Rainfall',
-		'color': '#89A54E',
-        'type': 'spline',
-        'data': [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
-        'tooltip': { 'valueSuffix': 'C' }
-	}
-	data4 = []
-	d = ChartData('name', 'Rainfall')
-	data4.append(d.__toDict__())
-	d = ChartData('color', '#89A54E')
-	data4.append(d.__toDict__())
-	d = ChartData('type', 'spline')
-	data4.append(d.__toDict__())
-	d = ChartData('data', [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6])
-	data4.append(d.__toDict__())
-	d = ChartData('tooltip', { 'valueSuffix': 'C' })
-	data4.append(d.__toDict__())
-	st = "zoomType: 'xy' "
-	print st
-	data5 = []
-	d1 = {'name' : 'Tokyo', 'data' : [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]}
-	d2 = {'name' : 'NewYork', 'data' : [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]}
-	d3 = []
-	d3.append(d1)
-	d3.append(d2)
-	d5 = {'series': d3}
-	print d5
-
-	d11 = {'chart' : "zoomType : 'xy'"}
-	d11['subtitle'] = {'text' : 'Source: WorldClimate.com'}
-	d11['title'] = {'text': 'Average Monthly Temperature and Rainfall in Tokyo'}
-	d11['xAxis'] = [{'categories': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
-					'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']}]
-	d21 = {'style' : {'color':'#89A54E'}}
-	d22 = {'format' : '{value}C'}
-	d23 = {'style' : {'color':'#89A54E'}}
-	d11['style'] = d23['style']
-	d11['chart2'] = {'chart' : {'zoomType' : 'xy'}}
-
-	d11['format'] = '{value}C'
-	#response_data['Tokyo'] = [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-	#response_data['NewYork'] = [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-	#response_data['Berlin'] = [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-	#response_data['London'] = [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-	return HttpResponse(json.dumps(d11), content_type="application/json")
-
-def highchart(request):
-	lu = { 'categories' : [ 'Fall 2008', 'Spring 2009','Fall 2009', 'Spring 2010', 'Fall 2010', 'Spring 2011'],\
-			'undergrad' : [18, 22, 30, 34, 40, 47],\
-			'grad' : [1, 2, 4, 4, 5, 7],\
-			'employee' : [2, 3, 0, 1, 1, 2] }
-	lu['total_enrolled'] = [sum(a) for a in zip(lu['undergrad'], lu['grad'],lu['employee'])]
-	return render_to_string('enrollment_chart.html', lu )
-
-class ChartData:
-	def __init__(self, iKey, iValue):
-		self.Key = iKey
-		self.Value = iValue
-
-	def __toDict__(self):
-		data = {}
-		data['Key'] = self.Key
-		data['Value'] = self.Value
-		return data
-
-
-class FileItem:
-    def __init__(self, fname):
-        self.fname = fname
-
-    def __repr__(self):
-        return json.dumps(self.__dict__)
