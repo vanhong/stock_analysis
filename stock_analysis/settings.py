18b35d1ae1f1205d495eccdd33848c86bf7e60d3
vanhong <vanhongkao@Kaoteki-MacBook-Pro.local>
2013-10-17 21:23:04 +0800

    131017 update drawCharts

--- stock_analysis/settings.py
+++ stock_analysis/settings.py
@@ -16,10 +16,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'stock_analysis',                      # Or path to database file if using sqlite3.
+        'NAME': 'stocks',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': 'vanhong',
+        'PASSWORD': 'vanhong',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
