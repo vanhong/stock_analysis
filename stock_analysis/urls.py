42d8ab08135ed70fe15dc63c7da28a78abfdcb0e
vanhong <vanhongkao@gmail.com>
2013-11-01 14:58:54 +0800

    131101 use beautifulsoup replace update monthRevenue, seasonProfit, Dividend by vk

--- stock_analysis/urls.py
+++ stock_analysis/urls.py
@@ -34,6 +34,7 @@ urlpatterns += patterns('stocks.views',
     (r'^update_month_revenue/$', 'update_month_revenue'),
     (r'^update_season_profit/$', 'update_season_profit'),
     (r'^update_season_revenue/$', 'update_season_revenue'),
+    (r'^stock/test/$', 'update_new_month_revenue'),
 )
 
 urlpatterns += patterns('financial.views',
