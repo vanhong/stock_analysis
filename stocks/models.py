5d5413fbe0fdf58143b9f2201a8bb47224cda399
vanhong <vanhongkao@gmail.com>
2013-11-18 00:02:24 +0800

    131117 update all by vk

--- stocks/models.py
+++ stocks/models.py
@@ -54,6 +54,7 @@ class SeasonProfit(models.Model):
 class Dividend(models.Model):
     surrogate_key = models.CharField(max_length=20, primary_key=True)
     year = models.IntegerField(db_index=True)
+    date = models.DateField(db_index=True)
     symbol = models.CharField(max_length=20, db_index=True)
     cash_dividends = models.DecimalField(max_digits=20, decimal_places=10, null=True)
     stock_dividends_from_retained_earnings = models.DecimalField(max_digits=20, decimal_places=10, null=True)
