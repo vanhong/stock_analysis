3f4c334234913fd24c64c8a78e2a5f897a5ca747
vanhong <vanhongkao@gmail.com>
2014-05-22 23:37:43 +0800

    update setting for merge

--- stock_analysis/settings.py
+++ stock_analysis/settings.py
@@ -18,8 +18,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'stocks',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'vanhong',
-        'PASSWORD': 'vanhong',
+        'USER': 'root',
+        'PASSWORD': '1111',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
